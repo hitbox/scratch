876df28395934173016a57367018292b0f51d1e6
Carl Harris <hitbox@ryzen.com>
2023-10-11 02:19:53 -0400

    pygame-examples/webbed.py mostly working again
    - switch to center again
    - trying to show the focus rect of the camera

--- pygame-examples/webbed.py
+++ pygame-examples/webbed.py
@@ -27,10 +27,13 @@ class Camera:
 
     def __init__(self, size):
         self.view = pygame.Rect((0, 0), size)
+        self.position = pygame.Vector2(self.view.center)
+        self.velocity = pygame.Vector2()
         x = -min(size) // 2
         self.focus = self.view.inflate(x, x)
-        self.position = pygame.Vector2(self.view.topleft)
-        self.velocity = pygame.Vector2()
+        self.focus.center = self.position
+        # TODO
+        # - trying to make the focus rect appear on screen where it should.
 
     def to_world(self, point):
         return self.position + point
@@ -38,10 +41,13 @@ class Camera:
     def to_screen(self, point):
         return point - self.position
 
+    def focus_as_screen(self):
+        return self.focus.move(*-self.position)
+
     def update(self, elapsed):
         self.position += self.velocity
-        self.view.topleft = self.position
-        self.focus.topleft = self.position
+        self.view.center = self.position
+        self.focus.center = self.position
 
 
 class EventMixin:
@@ -83,6 +89,7 @@ class DemoScene(EventMixin):
         self.clock = pygame.time.Clock()
         self.frames_per_second = 60
         self.camera = Camera(self.window.size)
+        self.ui_font = pygame.font.SysFont('monospace', 14)
         self.data = DemoData()
 
     def on_keydown(self, event):
@@ -108,28 +115,54 @@ class DemoScene(EventMixin):
             # move camera
             self.camera.position -= event.rel
 
-    def update(self):
-        # update state
-        elapsed = self.clock.tick(self.frames_per_second)
-        mouse_pos = pygame.mouse.get_pos()
-        # draw
-        self.screen.fill('black')
+    def lines_to_draw(self):
         lines = self.data.lines[:]
         if self.data.newline_start:
+            mouse_pos = pygame.mouse.get_pos()
             preview = Line(
                 self.data.newline_start,
                 self.camera.to_world(mouse_pos),
                 'white'
             )
             lines.append(preview)
-        for line in lines:
+        return lines
+
+    def update(self):
+        # update state and draw
+        elapsed = self.clock.tick(self.frames_per_second)
+        mouse_pos = pygame.mouse.get_pos()
+        self.camera.update(elapsed)
+        # draw
+        self.screen.fill('black')
+        # draw player
+        player = self.data.player
+        pos = -pygame.Vector2(self.camera.view.topleft)
+        self.screen.blit(player.image, player.rect.move(*pos))
+        # draw lines
+        for line in self.lines_to_draw():
             points = map(self.camera.to_screen, line)
             pygame.draw.line(self.screen, line.color, *points)
+        # draw camera
+        pygame.draw.rect(self.screen, 'magenta', self.camera.focus_as_screen(), 1)
+        # draw ui
+        cam = self.camera
+        text_lines = [
+            f'{cam.view=}',
+            f'{cam.focus=}',
+            f'{cam.position=}',
+        ]
+        images = [self.ui_font.render(line, True, 'azure') for line in text_lines]
+        rects = list(map(pygame.Surface.get_rect, images))
+        rects[0].bottomright = self.window.bottomright
+        setpairattr(rects, 'bottomright', 'topright')
+        for image, rect in zip(images, rects):
+            self.screen.blit(image, rect)
+        # draw cursor
         pygame.draw.circle(self.screen, 'azure', mouse_pos, 5, 2)
         pygame.display.flip()
 
 
-def pairattr(items, sattr, gattr):
+def setpairattr(items, sattr, gattr):
     for i1, i2 in zip(items, items[1:]):
         setattr(i2, sattr, getattr(i1, gattr))
 
