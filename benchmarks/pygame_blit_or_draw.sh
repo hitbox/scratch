5b0734b7cd3d89666dfd31933d50d8c9a0c83187
Carl Harris <hitbox@ryzen.com>
2023-11-27 03:27:39 -0500

    pygame blit vs draw benchmark
    - clean up code a little

--- benchmarks/pygame_blit_or_draw.sh
+++ benchmarks/pygame_blit_or_draw.sh
@@ -2,29 +2,34 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+screen_size="(500,)*2"
+draw_iterations="100"
+rect_size="(250,)*2"
+rect_color="'white'"
+
 echo draw
 echo
 python -m timeit \
     --setup "import pygame" \
-    --setup "screen = pygame.display.set_mode((500,)*2)" \
-    --setup "rect = pygame.Rect((0,)*2, (250,)*2)" \
+    --setup "screen = pygame.display.set_mode(${screen_size})" \
+    --setup "rect = pygame.Rect((0,)*2, ${rect_size})" \
     --setup "rect.center = screen.get_rect().center" \
     -- \
-    "for _ in range(100):" \
+    "for _ in range(${draw_iterations}):" \
     "    pygame.event.get()" \
-    "    pygame.draw.rect(screen, 'white', rect)" \
+    "    pygame.draw.rect(screen, ${rect_color}, rect)" \
     "    pygame.display.update()"
 
 echo blit
 echo
 python -m timeit \
     --setup "import pygame" \
-    --setup "screen = pygame.display.set_mode((500,)*2)" \
-    --setup "image = pygame.Surface((250,)*2)" \
-    --setup "image.fill('white')" \
+    --setup "screen = pygame.display.set_mode(${screen_size})" \
+    --setup "image = pygame.Surface(${rect_size})" \
+    --setup "image.fill(${rect_color})" \
     --setup "rect = image.get_rect(center=screen.get_rect().center)" \
     -- \
-    "for _ in range(100):" \
+    "for _ in range(${draw_iterations}):" \
     "    pygame.event.get()" \
     "    screen.blit(image, rect)" \
     "    pygame.display.update()"
