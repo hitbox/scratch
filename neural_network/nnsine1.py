4781fd6fbda8ce41aac860f1cb74f474202ba3e2
Carl Harris <hitbox@ryzen.com>
2023-11-27 03:26:40 -0500

    reorganize neural network things
    - added chatgpt output

---
+++ neural_network/nnsine1.py
@@ -0,0 +1,91 @@
+import random
+import math
+
+# Sigmoid activation function
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+# Derivative of the sigmoid function
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+# Initialize weights and biases
+def initialize_weights(input_size, hidden_size, output_size):
+    weights_input_hidden = [[random.uniform(-1, 1) for _ in range(hidden_size)] for _ in range(input_size)]
+    weights_hidden_output = [[random.uniform(-1, 1) for _ in range(output_size)] for _ in range(hidden_size)]
+    bias_hidden = [random.uniform(-1, 1) for _ in range(hidden_size)]
+    bias_output = [random.uniform(-1, 1) for _ in range(output_size)]
+    return weights_input_hidden, weights_hidden_output, bias_hidden, bias_output
+
+# Forward propagation
+def forward_propagation(inputs, weights_input_hidden, weights_hidden_output, bias_hidden, bias_output):
+    hidden_layer_input = [sum(inputs[i] * weights_input_hidden[i][j] for i in range(len(inputs))) for j in range(len(weights_input_hidden[0]))]
+    hidden_layer_output = [sigmoid(x + b) for x, b in zip(hidden_layer_input, bias_hidden)]
+    
+    output_layer_input = [sum(hidden_layer_output[i] * weights_hidden_output[i][j] for i in range(len(hidden_layer_output))) for j in range(len(weights_hidden_output[0]))]
+    output = [sigmoid(x + b) for x, b in zip(output_layer_input, bias_output)]
+    
+    return hidden_layer_output, output
+
+# Backpropagation
+def backpropagation(inputs, labels, hidden_layer_output, output, weights_input_hidden, weights_hidden_output, bias_hidden, bias_output):
+    error = [a - b for a, b in zip(labels, output)]
+    d_output = [e * sigmoid_derivative(o) for e, o in zip(error, output)]
+    
+    error_hidden = [sum(d_output[j] * weights_hidden_output[i][j] for j in range(len(d_output))) for i in range(len(hidden_layer_output))]
+    d_hidden = [e * sigmoid_derivative(o) for e, o in zip(error_hidden, hidden_layer_output)]
+    
+    # Update weights and biases
+    for i in range(len(weights_hidden_output)):
+        for j in range(len(weights_hidden_output[0])):
+            weights_hidden_output[i][j] += hidden_layer_output[i] * d_output[j]
+    
+    for i in range(len(weights_input_hidden)):
+        for j in range(len(weights_input_hidden[0])):
+            weights_input_hidden[i][j] += inputs[i] * d_hidden[j]
+    
+    bias_output = [b + d for b, d in zip(bias_output, d_output)]
+    bias_hidden = [b + d for b, d in zip(bias_hidden, d_hidden)]
+    
+    return weights_input_hidden, weights_hidden_output, bias_hidden, bias_output, error
+
+# Training the neural network for sine function approximation
+def train_neural_network(epochs):
+    input_size = 1  # One input value for sine function
+    hidden_size = 8  # Arbitrary hidden layer size
+    output_size = 1  # One output value (sine value)
+    
+    # Creating a dataset for sine function
+    inputs = [[i / 10] for i in range(-100, 100)]  # Input values from -10 to 10
+    labels = [[math.sin(i[0])] for i in inputs]  # Corresponding sine values
+    
+    # Initialize weights and biases
+    weights_input_hidden, weights_hidden_output, bias_hidden, bias_output = initialize_weights(input_size, hidden_size, output_size)
+    
+    for epoch in range(epochs):
+        total_error = 0
+        for i in range(len(inputs)):
+            hidden_layer_output, output = forward_propagation(inputs[i], weights_input_hidden, weights_hidden_output, bias_hidden, bias_output)
+        
+            weights_input_hidden, weights_hidden_output, bias_hidden, bias_output, error = backpropagation(
+                inputs[i], labels[i], hidden_layer_output, output, weights_input_hidden, weights_hidden_output, bias_hidden, bias_output)
+            
+            total_error += abs(error[0])  # Accumulating absolute error
+        
+        if epoch % 100 == 0:
+            print(f'Epoch {epoch} - Total Error: {total_error}')
+
+    return weights_input_hidden, weights_hidden_output, bias_hidden, bias_output
+
+# Making predictions for sine function
+def predict(inputs, weights_input_hidden, weights_hidden_output, bias_hidden, bias_output):
+    _, output = forward_propagation(inputs, weights_input_hidden, weights_hidden_output, bias_hidden, bias_output)
+    return output
+
+# Training the neural network
+trained_weights = train_neural_network(epochs=1000)
+
+# Predicting sine values using the trained network
+input_value = [0.5]  # Example input value for prediction
+predicted_output = predict(input_value, *trained_weights)
+print(f"Sine value for input {input_value[0]}: {predicted_output[0]}")
---
+++ neural_network/small_language_model.original.py
@@ -0,0 +1,106 @@
+import numpy as np
+
+class CharRNN:
+    def __init__(self, hidden_size, seq_length, learning_rate):
+        self.hidden_size = hidden_size
+        self.seq_length = seq_length
+        self.learning_rate = learning_rate
+        
+        self.chars = list('abcdefghijklmnopqrstuvwxyz ')
+        self.char_to_idx = {ch: i for i, ch in enumerate(self.chars)}
+        self.idx_to_char = {i: ch for i, ch in enumerate(self.chars)}
+        self.vocab_size = len(self.chars)
+        
+        self.Wxh = np.random.randn(hidden_size, self.vocab_size) * 0.01
+        self.Whh = np.random.randn(hidden_size, hidden_size) * 0.01
+        self.Why = np.random.randn(self.vocab_size, hidden_size) * 0.01
+        self.bh = np.zeros((hidden_size, 1))
+        self.by = np.zeros((self.vocab_size, 1))
+        
+        self.h_prev = np.zeros((hidden_size, 1))
+    
+    def forward(self, inputs):
+        xs, hs, ys, ps = {}, {}, {}, {}
+        hs[-1] = np.copy(self.h_prev)
+        
+        for t in range(len(inputs)):
+            xs[t] = np.zeros((self.vocab_size, 1))
+            xs[t][self.char_to_idx[inputs[t]]] = 1
+            hs[t] = np.tanh(np.dot(self.Wxh, xs[t]) + np.dot(self.Whh, hs[t - 1]) + self.bh)
+            ys[t] = np.dot(self.Why, hs[t]) + self.by
+            ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t]))
+        
+        return xs, hs, ys, ps
+    
+    def backward(self, inputs, targets, xs, hs, ps):
+        dWxh, dWhh, dWhy = np.zeros_like(self.Wxh), np.zeros_like(self.Whh), np.zeros_like(self.Why)
+        dbh, dby = np.zeros_like(self.bh), np.zeros_like(self.by)
+        dh_next = np.zeros_like(hs[0])
+        
+        for t in reversed(range(len(inputs))):
+            dy = np.copy(ps[t])
+            dy[self.char_to_idx[targets[t]]] -= 1
+            dWhy += np.dot(dy, hs[t].T)
+            dby += dy
+            dh = np.dot(self.Why.T, dy) + dh_next
+            dhraw = (1 - hs[t] * hs[t]) * dh
+            dbh += dhraw
+            dWxh += np.dot(dhraw, xs[t].T)
+            dWhh += np.dot(dhraw, hs[t - 1].T)
+            dh_next = np.dot(self.Whh.T, dhraw)
+        
+        for dparam in [dWxh, dWhh, dWhy, dbh, dby]:
+            np.clip(dparam, -5, 5, out=dparam)
+        
+        self.Wxh -= self.learning_rate * dWxh
+        self.Whh -= self.learning_rate * dWhh
+        self.Why -= self.learning_rate * dWhy
+        self.bh -= self.learning_rate * dbh
+        self.by -= self.learning_rate * dby
+        
+        self.h_prev = hs[len(inputs) - 1]
+    
+    def train(self, data, epochs):
+        for epoch in range(epochs):
+            ptr = 0
+            for _ in range(len(data) // self.seq_length):
+                inputs = [ch for ch in data[ptr:ptr+self.seq_length]]
+                targets = [ch for ch in data[ptr+1:ptr+self.seq_length+1]]
+                
+                if len(inputs) == self.seq_length:
+                    xs, hs, ys, ps = self.forward(inputs)
+                    self.backward(inputs, targets, xs, hs, ps)
+                    
+                ptr += self.seq_length
+            
+            if epoch % 100 == 0:
+                print(f'Epoch: {epoch}/{epochs}')
+    
+    def sample(self, seed, n):
+        idx = self.char_to_idx[seed]
+        x = np.zeros((self.vocab_size, 1))
+        x[idx] = 1
+        h = np.copy(self.h_prev)
+        generated_text = seed
+        
+        for _ in range(n):
+            h = np.tanh(np.dot(self.Wxh, x) + np.dot(self.Whh, h) + self.bh)
+            y = np.dot(self.Why, h) + self.by
+            p = np.exp(y) / np.sum(np.exp(y))
+            
+            idx = np.random.choice(range(self.vocab_size), p=p.ravel())
+            x = np.zeros((self.vocab_size, 1))
+            x[idx] = 1
+            generated_text += self.idx_to_char[idx]
+        
+        return generated_text
+
+# Example usage:
+if __name__ == "__main__":
+    data = "hello world how are you doing today"
+    rnn = CharRNN(hidden_size=100, seq_length=25, learning_rate=0.1)
+    rnn.train(data, epochs=1000)
+    
+    generated_text = rnn.sample(seed='h', n=50)
+    print("Generated Text:")
+    print(generated_text)
