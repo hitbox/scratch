1c8a69db0f0e712e305b065aefc6e1e30771103e
Carl Harris <hitbox@ryzen.com>
2023-12-14 12:36:23 -0500

    pygame/dicepips.py: cleaning and playing
    - trying to put things into generic functions
    - thinking about a system to render like a browser that picks up styling
      information from an object

--- pygame/dicepips.py
+++ pygame/dicepips.py
@@ -6,11 +6,25 @@ import os
 import random
 
 from itertools import groupby
+from itertools import permutations
 from operator import itemgetter
 from string import digits
+from types import SimpleNamespace
 
 with contextlib.redirect_stdout(open(os.devnull, 'w')):
     import pygame
+
+class Style(SimpleNamespace):
+
+    def __getattr__(self, key):
+        return self.__dict__.get(key)
+
+    def __getitem__(self, key):
+        return self.__dict__.get(key)
+
+    def __setitem__(self, key, value):
+        setattr(self, key, value)
+
 
 def deltas():
     # generate 3x3 neighbors deltas
@@ -23,56 +37,66 @@ def pip_order(delta):
     is_corner = all(delta)
     return (-is_corner, -col, row)
 
-def delta_angle(delta):
+def pip_angle(delta):
     row, col = delta
     angle = math.atan2(row, col) % math.pi
     return angle
 
-def paired(deltas):
-    deltas = sorted(deltas, key=delta_angle)
-    grouped = groupby(deltas, key=delta_angle)
-    for _key, pairs in grouped:
-        delta1, delta2 = pairs
-        yield (delta1, delta2)
-        yield (delta2, delta1)
+def sorted_groupby(iterable, key=None, reverse=False):
+    return groupby(sorted(iterable, key=key, reverse=reverse), key=key)
+
+def update_with_reverse(dict_):
+    dict_.update(list(map(reversed, dict_.items())))
+
+def iteritems(iterable, *indexes):
+    return map(itemgetter(*indexes), iterable)
+
+def mapgroupiters(grouped, container=tuple):
+    """
+    containerize the group iterables as from itertools.groupby
+    """
+    return map(container, iteritems(grouped, 1))
+
+def anyin(iterable, *subs):
+    return any(sub in iterable for sub in subs)
 
 def colorful(name):
     return (
-        'grey' not in name
-        and
-        'gray' not in name
-        and
-        'white' not in name
-        and
-        'black' not in name
+        not anyin(name, 'grey', 'gray', 'white', 'black')
         and
         set(name).isdisjoint(digits)
+        and
+        name not in ('red', 'green', 'blue')
     )
 
 COLORFUL = {name: rgba for name, rgba in pygame.color.THECOLORS.items() if colorful(name)}
 
 DELTAS_WITH_ORIGIN = list(deltas())
 
-DELTAS = sorted(DELTAS_WITH_ORIGIN, key=pip_order)
-DELTAS.remove((0,0))
+PIP_DELTAS = sorted(DELTAS_WITH_ORIGIN, key=pip_order)
+PIP_DELTAS.remove((0,0))
 
-OPPOSITES = dict(paired(DELTAS))
+OPPOSITES = dict(mapgroupiters(sorted_groupby(PIP_DELTAS, key=pip_angle)))
+update_with_reverse(OPPOSITES)
+
+def is_odd(n):
+    return n % 2 != 0
 
 def iter_pip_deltas(n):
-    if n % 2 != 0:
+    if is_odd(n):
         yield (0, 0)
         n -= 1
         if n == 0:
             return
     seen = set()
-    for delta in DELTAS:
+    for delta in PIP_DELTAS:
         opposite = OPPOSITES[delta]
         if delta in seen or opposite in seen:
             continue
-        yield delta
-        yield opposite
         seen.add(delta)
         seen.add(opposite)
+        yield delta
+        yield opposite
         if len(seen) == n:
             break
 
@@ -83,42 +107,68 @@ def mix(a, b, x):
 def remap(a, b, c, d, x):
     return x*(d-c)/(b-a) + c-a*(d-c)/(b-a)
 
-def iterdice():
-    diceiter = sorted(enumerate(DELTAS_WITH_ORIGIN, start=1), key=itemgetter(0))
-    for color, (n, (row, col)) in zip(COLORFUL, diceiter):
-        yield (n, color, (row, col))
+def iter_rect_ends(rect):
+    # generate the dimensional extremes of a rect, its x and y endpoints
+    yield (rect.left, rect.right)
+    yield (rect.top, rect.bottom)
 
-def dicerect(dice_window, rowcol, dice_size, dice_border):
+def remap_delta_xy(rowcol, rect):
+    def _remap_dim(item):
+        index, rectdim = item
+        min_, max_ = rectdim
+        return remap(-1, +1, min_, max_, index)
+    pos = map(_remap_dim, zip(reversed(rowcol), iter_rect_ends(rect)))
+    return tuple(pos)
+
+def make_dicerect(dice_window, rowcol, dice_size, dice_border):
     row, col = rowcol
     # map from delta to screen/rect position
-    x = remap(
-        -1, 1,
-        dice_window.left + dice_border, dice_window.right - dice_size - dice_border,
-        col
+    remap_rect = dice_window.inflate((-(dice_border + dice_size),)*2)
+    center = remap_delta_xy(rowcol, remap_rect)
+    dice_rect = pygame.Rect(0, 0, dice_size, dice_size)
+    dice_rect.center = center
+    return dice_rect
+
+    remap_rect = pygame.Rect(
+        dice_window.left + dice_border,
+        dice_window.top + dice_border,
+        dice_window.width - dice_size - dice_border * 2,
+        dice_window.height - dice_size - dice_border * 2,
     )
-    y = remap(
-        -1, 1,
-        dice_window.top + dice_border, dice_window.bottom - dice_size - dice_border,
-        row
-    )
+    x, y = remap_delta_xy(rowcol, remap_rect)
     dice_rect = pygame.Rect(x, y, dice_size, dice_size)
     return dice_rect
 
-def divrect(rect, denom, pin='topleft'):
-    # NOTES
-    # - was trying to make a func that would resize a rect so that a circle
-    #   could be drawn inside it that would look like a shadow
-    _rect = rect.copy()
-    _rect.width /= denom
-    _rect.height /= denom
-    setattr(_rect, pin, getattr(rect, pin))
-    return _rect
+def piprect(dice_rect, pipsize, pippos, dice_border):
+    """
+    Rect for one of the pip positions inside a dice rect.
+    """
+    pip_width, pip_height = pipsize
+    pip_row, pip_col = pippos
+    x = remap(
+        -1,
+        +1,
+        dice_rect.left + dice_border,
+        dice_rect.right - pip_width - dice_border,
+        pip_col,
+    )
+    y = remap(
+        -1,
+        +1,
+        dice_rect.top + dice_border,
+        dice_rect.bottom - pip_height - dice_border,
+        pip_row,
+    )
+    pip_rect = pygame.Rect(x, y, pip_width, pip_height)
+    return pip_rect
 
 def run():
     pygame.font.init()
-    screen = pygame.display.set_mode((800,)*2)
+    screen = pygame.display.set_mode((800, 700))
     window = screen.get_rect()
     font = pygame.font.SysFont('monospace', 24)
+    clock = pygame.time.Clock()
+    framerate = 60
 
     reduction = -min(window.size) // 16
     dice_window = window.inflate((reduction,)*2)
@@ -132,8 +182,13 @@ def run():
     # - denominator here dialed in manually
     dice_size = min(dice_window.size) // 3.05 - dice_margin
 
+    dice_colors = random.sample(list(COLORFUL), 9)
+
+    dice_list = list(range(1,10))
+
     running = True
     while running:
+        elapsed = clock.tick(framerate)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -143,46 +198,29 @@ def run():
         screen.fill('black')
         pygame.draw.rect(screen, 'brown', dice_window, 1)
 
-        for n, color, (dice_row, dice_col) in iterdice():
-            dice_rect = dicerect(
-                dice_window,
-                (dice_row, dice_col),
-                dice_size,
-                dice_margin,
-            )
+        for n, dicepos, color in zip(dice_list, DELTAS_WITH_ORIGIN, dice_colors):
+            dice_rect = make_dicerect(dice_window, dicepos, dice_size, dice_margin)
             # fill
-            pygame.draw.rect(screen, 'brown', dice_rect)
+            pygame.draw.rect(screen, color, dice_rect)
             # border
-            pygame.draw.rect(screen, 'white', dice_rect, border_width)
+            pygame.draw.rect(screen, 'azure', dice_rect, border_width)
 
             dice_border = min(dice_rect.size) // space_ratio_denom
 
             pip_width = (dice_rect.width // 3) - dice_border
             pip_height = (dice_rect.height // 3) - dice_border
-            for (pip_row, pip_col) in iter_pip_deltas(n):
-                x = remap(
-                    -1,
-                    +1,
-                    dice_rect.left + dice_border,
-                    dice_rect.right - pip_width - dice_border,
-                    pip_col,
-                )
-                y = remap(
-                    -1,
-                    +1,
-                    dice_rect.top + dice_border,
-                    dice_rect.bottom - pip_height - dice_border,
-                    pip_row,
-                )
-                pip_rect = pygame.Rect(x, y, pip_width, pip_height)
+            pip_size = (pip_width, pip_height)
+            for pippos in iter_pip_deltas(n):
+                pip_rect = piprect(dice_rect, pip_size, pippos, dice_border)
                 radius = min(pip_rect.size) // 2
                 # fill
-                pygame.draw.circle(screen, 'white', pip_rect.center, radius)
-                # border with fake shading
+                pygame.draw.circle(screen, 'azure', pip_rect.center, radius)
+                # pip border with fake shading
                 pygame.draw.circle(screen, 'grey', pip_rect.center, radius, border_width)
                 pygame.draw.circle(screen, 'black', pip_rect.center, radius, border_width//2)
 
-            text_image = font.render(f'{n=}', True, 'white')
+            # dice number text
+            text_image = font.render(f'{n=}', True, 'azure')
             screen.blit(text_image, text_image.get_rect(midtop=dice_rect.midbottom))
 
         pygame.display.flip()
