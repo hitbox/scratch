dfceb8c5901220bb9125f58455de84f38cedab90
Carl Harris <hitbox@ryzen.com>
2024-01-08 02:53:13 -0500

    pygame/negative_rect.py
    - messing around trying to improve

--- pygame/negative_rect.py
+++ pygame/negative_rect.py
@@ -40,11 +40,12 @@ class Engine:
 
 class Demo:
 
-    def __init__(self, scale, framerate, fontsize):
+    def __init__(self, scale, framerate, fontsize, constraint_margins=False):
         self.scale = scale
         self.framerate = framerate
         self.fontsize = fontsize
         self.clock = pygame.time.Clock()
+        self.constraint_margins = constraint_margins
 
     def start(self, engine):
         self.engine = engine
@@ -57,12 +58,7 @@ class Demo:
         self.buffer = pygame.Surface(size)
         self.world = self.buffer.get_rect()
         self.space = self.world.inflate((-min(self.world.size)//3,)*2)
-        self.walls = [
-            make_rect(self.space, bottom=self.space.top),
-            make_rect(self.space, left=self.space.right),
-            make_rect(self.space, top=self.space.bottom),
-            make_rect(self.space, right=self.space.left),
-        ]
+        self.walls = list(surrounding(self.space))
 
         refrect = self.space.inflate((-min(self.space.size)//1.25,)*2)
         refrect.normalize()
@@ -82,38 +78,18 @@ class Demo:
         # create labels
         text_blits = []
         for rect, color in zip(margins, COLORS):
-            pygame.draw.rect(self.buffer, color, rect, 1)
-            text = f'{rect.width}x{rect.height}={rect.width*rect.height}'
-            image = self.font.render(text, True, 'azure')
+            text = f'{rect.width}x{rect.height}={area(rect):,}'
+            image = self.font.render(text, True, color)
             rect = image.get_rect(center=rect.center)
             text_blits.append((image, rect))
         # separate text labels
-        for _ in range(1):
-            rects = list(rect for _, rect in text_blits)
-            rects += self.walls
-            overlaps = [
-                (r1, r2, r1.clip(r2))
-                for r1, r2 in it.combinations(rects, 2)
-                if r1.colliderect(r2)
-            ]
-            if not any(overlaps):
-                break
-            for r1, r2, overlap in overlaps:
-                direction = pygame.Vector2(r2.center) - pygame.Vector2(r1.center)
-                direction.normalize()
-                overlap = pygame.Vector2(overlap.size)
-                move = pygame.Vector2(overlap * direction)
-                if r1 in self.walls:
-                    r2.topleft -= move
-                elif r2 in self.walls:
-                    r1.topleft -= move
-                else:
-                    r1.topleft -= move / 2
-                    r2.topleft += move / 2
+        rects = list(rect for _, rect in text_blits)
+        resolve_overlaps(rects, self.walls, 100)
         # drawing
-        self.clear_screen()
-        self.draw_draggables()
-        self.draw_margins(margins)
+        self.clear()
+        draw_rects(self.buffer, it.repeat('lime'), self.walls, width=0)
+        draw_rects(self.buffer, it.repeat('purple4'), self.draggables, width=1)
+        draw_rects(self.buffer, COLORS, margins, width=1)
         # draw text labels
         for (image, rect), color in zip(text_blits, COLORS):
             pygame.draw.circle(self.buffer, color, rect.center, 2)
@@ -121,6 +97,7 @@ class Demo:
         # scale and update display
         pygame.transform.scale(self.buffer, self.window.size, self.screen)
         pygame.display.flip()
+        # events
         for event in pygame.event.get():
             event_name = pygame.event.event_name(event.type)
             method_name = f'do_{event_name.lower()}'
@@ -151,32 +128,24 @@ class Demo:
         if not (event.buttons[0] and self.dragging):
             return
         # left button down and moving
-        delta = pygame.Vector2(*map(lambda x: x / self.scale, event.rel))
-        # validate new position for margin area size
-        testrect = pygame.Rect(self.dragging.topleft + delta, self.dragging.size)
-        _margins = map(pygame.Rect, margin_rects([testrect], self.space))
-        # all areas > x and there are valid margin rects all around
-        valid_move = all(
-            validate_inside(rect, self.space) and area(rect) > 1000
-            for rect in _margins
-        )
-        if valid_move:
+        screen_rel = event.rel
+        delta = pygame.Vector2(*map(lambda x: x / self.scale, screen_rel))
+        if not self.constraint_margins:
             self.dragging.topleft += delta
+        else:
+            # validate new position for margin area size
+            testrect = pygame.Rect(self.dragging.topleft + delta, self.dragging.size)
+            _margins = map(pygame.Rect, margin_rects([testrect], self.space))
+            # all areas > x and there are valid margin rects all around
+            valid_move = all(
+                validate_inside(rect, self.space) and area(rect) > 1000
+                for rect in _margins
+            )
+            if valid_move:
+                self.dragging.topleft += delta
 
-    def clear_screen(self):
+    def clear(self):
         self.buffer.fill('black')
-
-    def draw_walls(self):
-        for rect in self.walls:
-            pygame.draw.rect(self.buffer, 'lime', rect, 1)
-
-    def draw_draggables(self):
-        for rect in self.draggables:
-            pygame.draw.rect(self.buffer, 'purple4', rect, 1)
-
-    def draw_margins(self, margins):
-        for rect, color in zip(margins, COLORS):
-            pygame.draw.rect(self.buffer, color, rect, 1)
 
 
 def make_rect(rect=None, **kwargs):
@@ -194,12 +163,22 @@ def rect_from_points(x1, y1, x2, y2):
     return (x1, y1, w, h)
 
 def aggsides(func, *rects):
+    """
+    execute `func` on all sides of all rects returning unpackable of
+    `func(tops), func(rights), func(bottoms), func(lefts)`
+    """
     return map(func, *map(sides, rects))
 
 def minsides(*rects):
+    """
+    minimum of all four rects' sides
+    """
     return aggsides(min, *rects)
 
 def maxsides(*rects):
+    """
+    maximum of all four rects' sides
+    """
     return aggsides(max, *rects)
 
 def iter_rect_diffs(rect, inside):
@@ -222,6 +201,16 @@ def iter_rect_diffs(rect, inside):
     # left
     yield rect_from_points(inside.left, maxtop, rect.left, minbottom)
 
+def surrounding(rect):
+    yield make_rect(size=rect.size, bottomright=rect.topleft)
+    yield make_rect(size=rect.size, midbottom=rect.midtop)
+    yield make_rect(size=rect.size, bottomleft=rect.topright)
+    yield make_rect(size=rect.size, midleft=rect.midright)
+    yield make_rect(size=rect.size, topleft=rect.bottomright)
+    yield make_rect(size=rect.size, midtop=rect.midbottom)
+    yield make_rect(size=rect.size, topright=rect.bottomleft)
+    yield make_rect(size=rect.size, midright=rect.midleft)
+
 def validate_inside(rect, inside):
     _, _, w, h = rect
     return 0 < w < inside.width and 0 < h < inside.height
@@ -234,8 +223,59 @@ def margin_rects(rects, inside):
 def area(rect):
     return rect.width * rect.height
 
+def overlapping(rects):
+    for r1, r2 in it.combinations(rects, 2):
+        if r1.colliderect(r2):
+            yield (r1, r2)
+
+def resolve_overlaps(rects, immovable, maxsteps):
+    for _ in range(maxsteps):
+        overlaps = [
+            (r1, r2) for r1, r2 in overlapping(rects)
+        ]
+        if not overlaps:
+            break
+        for r1, r2 in overlaps:
+            if r1 in immovable and r2 in immovable:
+                continue
+            elif r1 in immovable:
+                resolve_from_immovable(r2, r1)
+            elif r2 in immovable:
+                resolve_from_immovable(r1, r2)
+            else:
+                delta = pygame.Vector2(r1.center) - r2.center
+                overlap = pygame.Vector2(r1.clip(r2).size)
+                delta.scale_to_length(overlap.length())
+                r1.center += delta / 2
+                r2.center -= delta / 2
+
+def resolve_from_immovable(rect, immovable):
+    """
+    Move rect outside of the immovable rect.
+    """
+    overlap = rect.clip(immovable)
+    if overlap.width > 0 and overlap.height > 0:
+        dx, dy = overlap.size
+        if dx < dy:
+            if rect.centerx < immovable.centerx:
+                rect.right = immovable.left
+            else:
+                rect.left = immovable.right
+        else:
+            if rect.centery < immovable.centery:
+                rect.bottom = immovable.top
+            else:
+                rect.top = immovable.bottom
+
+def draw_rects(surf, colors, rects, **kwargs):
+    for color, rect in zip(colors, rects):
+        pygame.draw.rect(surf, color, rect, **kwargs)
+
 def size_type(string):
-    return tuple(map(int, string.replace(',', ' ').split()))
+    size = tuple(map(int, string.replace(',', ' ').split()))
+    if len(size) < 2:
+        size *= 2
+    return size
 
 def main(argv=None):
     parser = argparse.ArgumentParser()
