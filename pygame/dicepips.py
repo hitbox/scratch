fb07083745f49da380f1e677a1616701a2704d5b
Carl Harris <hitbox@ryzen.com>
2024-01-31 03:30:26 -0500

    pygame/dicepips.py
    - reduced complexity to showing one die at a time

--- pygame/dicepips.py
+++ pygame/dicepips.py
@@ -13,22 +13,9 @@ from pygamelib import pygame
 
 class DiceDemo(pygamelib.DemoBase):
 
-    def __init__(self, font):
-        self.font = font
-
-    def start(self, engine):
-        super().start(engine)
-        self.dice_list = list(range(1,10))
-        self.border_width = 4
-        self.space_ratio_denom = 2 ** 4
-
-        reduction = -min(self.window.size) // 16
-        self.dice_window = self.window.inflate((reduction,)*2)
-        self.dice_margin = min(self.dice_window.size) // self.space_ratio_denom
-        # NOTE
-        # - denominator here dialed in manually
-        self.dice_size = min(self.dice_window.size) // 3.05 - self.dice_margin
-        self.dice_colors = random.sample(list(COLORFUL), 9)
+    def __init__(self, images, rects):
+        self.images = images
+        self.rects = rects
 
     def do_quit(self, event):
         self.engine.stop()
@@ -42,32 +29,8 @@ class DiceDemo(pygamelib.DemoBase):
 
     def draw(self):
         self.screen.fill('black')
-        pygame.draw.rect(self.screen, 'brown', self.dice_window, 1)
-        for npips, dicepos, color in zip(self.dice_list, DELTAS_WITH_ORIGIN, self.dice_colors):
-            dice_rect = make_dicerect(self.dice_window, dicepos, self.dice_size, self.dice_margin)
-            # fill
-            pygame.draw.rect(self.screen, color, dice_rect)
-            # border
-            pygame.draw.rect(self.screen, 'azure', dice_rect, self.border_width)
-
-            dice_border = min(dice_rect.size) // self.space_ratio_denom
-
-            pip_width = (dice_rect.width // 3) - dice_border
-            pip_height = (dice_rect.height // 3) - dice_border
-            pip_size = (pip_width, pip_height)
-            for pippos in iter_pip_deltas(npips):
-                pip_rect = piprect(dice_rect, pip_size, pippos, dice_border)
-                radius = min(pip_rect.size) // 2
-                # fill
-                pygame.draw.circle(self.screen, 'azure', pip_rect.center, radius)
-                # pip border with fake shading
-                pygame.draw.circle(self.screen, 'grey', pip_rect.center, radius, self.border_width)
-                pygame.draw.circle(self.screen, 'black', pip_rect.center, radius, self.border_width//2)
-
-            # dice number text
-            text_image = self.font.render(f'{npips=}', True, 'azure')
-            self.screen.blit(text_image, text_image.get_rect(midtop=dice_rect.midbottom))
-
+        for image, rect in zip(self.images, self.rects):
+            self.screen.blit(image, rect)
         pygame.display.flip()
 
 
@@ -99,20 +62,6 @@ def mapgroupiters(grouped, container=tuple):
     """
     return map(container, iteritems(grouped, 1))
 
-def anyin(iterable, *subs):
-    return any(sub in iterable for sub in subs)
-
-def colorful(name):
-    return (
-        not anyin(name, 'grey', 'gray', 'white', 'black')
-        and
-        set(name).isdisjoint(digits)
-        and
-        name not in ('red', 'green', 'blue')
-    )
-
-COLORFUL = {name: rgba for name, rgba in pygame.color.THECOLORS.items() if colorful(name)}
-
 DELTAS_WITH_ORIGIN = list(deltas())
 
 PIP_DELTAS = sorted(DELTAS_WITH_ORIGIN, key=pip_order)
@@ -125,6 +74,8 @@ def is_odd(n):
     return n % 2 != 0
 
 def iter_pip_deltas(n):
+    # TODO
+    # - more than nine pips
     if is_odd(n):
         yield (0, 0)
         n -= 1
@@ -187,12 +138,34 @@ def piprect(dice_rect, pipsize, pippos, dice_border):
     pip_rect = pygame.Rect(x, y, pip_width, pip_height)
     return pip_rect
 
-def run(display_size):
-    font = pygamelib.monospace_font(24)
-    state = DiceDemo(font)
+def draw_pip(surf, rect, border_width):
+    radius = min(rect.size) // 2
+    # fill
+    pygame.draw.circle(surf, 'azure', rect.center, radius)
 
+def draw_die(surf, npips, border):
+    rect = surf.get_rect()
+    pip_size = (
+        rect.width // 3 - border,
+        rect.height // 3 - border,
+    )
+    for pippos in iter_pip_deltas(npips):
+        pip_rect = piprect(rect, pip_size, pippos, border)
+        draw_pip(surf, pip_rect, border)
+
+def run(display_size, npips):
+    window = pygame.Rect((0,0), display_size)
+
+    die_size = (min(display_size) // 2,)*2
+    die_surf = pygame.Surface(die_size)
+    die_surf.fill('firebrick')
+    border = min(die_size) // 16
+    draw_die(die_surf, npips, border)
+    pygame.draw.rect(die_surf, 'azure', die_surf.get_rect(), border // 2)
+    die_rect = die_surf.get_rect(center=window.center)
+
+    state = DiceDemo([die_surf], [die_rect])
     pygame.display.set_mode(display_size)
-
     engine = pygamelib.Engine()
     engine.run(state)
 
@@ -201,8 +174,9 @@ def main(argv=None):
     Draw dice pips algorithm
     """
     parser = pygamelib.command_line_parser()
+    parser.add_argument('npips', type=int)
     args = parser.parse_args(argv)
-    run(args.display_size)
+    run(args.display_size, args.npips)
 
 if __name__ == '__main__':
     main()
