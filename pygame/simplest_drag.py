de78d8af1af0720a9799a46b1b1ce65403694fa4
Carl Harris <hitbox@ryzen.com>
2024-03-11 04:06:24 -0400

    pygame/simplest_drag.py
    - added main func
    - silence pygame
    - better argument parser
    - updated notes

--- pygame/simplest_drag.py
+++ pygame/simplest_drag.py
@@ -1,7 +1,32 @@
-import pygame
+import argparse
 
-screen = pygame.display.set_mode((800,)*2)
-rect = screen.get_rect().inflate((-600,)*2)
+import pygamelib
+
+from pygamelib import pygame
+
+def main(argv=None):
+    parser = pygamelib.command_line_parser(
+        description = 'Demo simplest possible dragging with pygame.',
+    )
+    args = parser.parse_args(argv)
+
+    screen = pygame.display.set_mode(args.display_size)
+    rect = screen.get_rect().inflate((-600,)*2)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEMOTION:
+                if event.buttons[0] and rect.collidepoint(event.pos):
+                    rect.topleft += pygame.Vector2(event.rel)
+        screen.fill('black')
+        pygame.draw.rect(screen, 'orange', rect, 0)
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    main()
 
 # 2024-03-04 Mon.
 # - trying to think of the simplest possible way to drag
@@ -9,15 +34,7 @@ rect = screen.get_rect().inflate((-600,)*2)
 # - maybe maintain a collection of draggables and which ones were initially
 #   clicked and only those are considered dragging
 # - other problem is you can drag too fast and lose the dragging
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.MOUSEMOTION:
-            if event.buttons[0] and rect.collidepoint(event.pos):
-                rect.topleft += pygame.Vector2(event.rel)
-    screen.fill('black')
-    pygame.draw.rect(screen, 'orange', rect, 0)
-    pygame.display.flip()
+# 2024-03-11 Mon.
+# - added main func
+# - silence pygame with pygamelib
+# - use pygamelib more
