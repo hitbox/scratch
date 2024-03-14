263274674488f15bac1b6a2824ced092d34f0365
Carl Harris <hitbox@ryzen.com>
2024-03-14 02:50:13 -0400

    l_system
    - added example run
    - cleanup code
    - dark mode colors

---
+++ pygame/examples/l_system.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+python l_system.py --length 5 --startx -200 --starty -200 --iterations 5
--- pygame/l_system.py
+++ pygame/l_system.py
@@ -1,17 +1,18 @@
 import argparse
 import turtle
 
+import pygamelib
+
 def apply_rules(symbols):
-    new_symbols = ""
+    new_symbols = ''
     for symbol in symbols:
-        if symbol == "F":
-            new_symbols += "F+F-F-F+F"
+        if symbol == 'F':
+            new_symbols += 'FLFRFRFLF'
         else:
             new_symbols += symbol
     return new_symbols
 
-def generate_l_system(axiom, iterations):
-    current_symbols = axiom
+def generate_l_system(current_symbols, iterations):
     for _ in range(iterations):
         current_symbols = apply_rules(current_symbols)
     return current_symbols
@@ -19,25 +20,35 @@ def generate_l_system(axiom, iterations):
 def draw_l_system(symbols, length, angle):
     turtle.speed(0)
     for symbol in symbols:
-        if symbol == "F":
+        if symbol == 'F':
             turtle.forward(length)
-        elif symbol == "+":
+        elif symbol == 'L':
             turtle.left(angle)
-        elif symbol == "-":
+        elif symbol == 'R':
             turtle.right(angle)
 
 def main(argv=None):
-    parser = argparse.ArgumentParser()
+    parser = pygamelib.command_line_parser()
+    parser.add_argument('--startx', type=int, default=0)
+    parser.add_argument('--starty', type=int, default=0)
     parser.add_argument('--iterations', type=int, default=4)
+    parser.add_argument('--length', type=int, default=2)
+    parser.add_argument('--angle', type=int, default=90)
     args = parser.parse_args(argv)
-    axiom = "F"
-    l_system = generate_l_system(axiom, args.iterations)
 
-    length = 2
-    angle = 90
-    draw_l_system(l_system, length, angle)
+    turtle.setup(*args.display_size)
+    turtle.bgcolor('black')
+    turtle.pencolor('white')
+
+    turtle.penup()
+    turtle.setposition(args.startx, args.starty)
+    turtle.pendown()
+
+    axiom = 'F'
+    l_system = generate_l_system(axiom, args.iterations)
+    draw_l_system(l_system, args.length, args.angle)
 
     turtle.done()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
