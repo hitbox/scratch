b6995abe1646f4558864fbe84ecbf96b85884be7
Carl Harris <hitbox@ryzen.com>
2024-03-20 05:33:06 -0400

    init template

---
+++ pygame/template.py
@@ -0,0 +1,10 @@
+import pygamelib
+
+from pygamelib import pygame
+
+def main(argv=None):
+    parser = pygamelib.command_line_parser()
+    args = parser.parse_args(argv)
+
+if __name__ == '__main__':
+    main()
