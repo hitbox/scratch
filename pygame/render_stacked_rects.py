05495ab349f94a78ca84b49e942d249f9dc45534
Carl Harris <hitbox@ryzen.com>
2024-03-26 12:23:39 -0400

    pygame/render_stacked_rects.py
    - init idea for future

---
+++ pygame/render_stacked_rects.py
@@ -0,0 +1,4 @@
+# TODO
+# - some way to render overlapping rects so they're all visible on screen
+# - maybe order the rect by "how covered it is" with bottom-most rects brought
+#   to the top (the end of the iterable)
