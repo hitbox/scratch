397e6b05453141a369de581be23ca48880ed26d7
Carl Harris <hitbox@ryzen.com>
2024-04-03 02:31:07 -0400

    benchmarks/minmax_sorted_flip.sh
    - added titles for output

--- benchmarks/minmax_sorted_flip.sh
+++ benchmarks/minmax_sorted_flip.sh
@@ -5,9 +5,16 @@ IFS=$'\n\t'
 SETUP="a = 2; b = 1"
 ASSERT="assert a < b"
 
+echo "min(), max() and unpack"
 python -m timeit --setup $SETUP -- "a, b = min(a, b), max(a, b)" $ASSERT
+
+echo "pygame.Rect.normalize"
 python -m timeit --setup "import pygame" -- "r = pygame.Rect(0, 0, -10, -10)" "r.normalize()"
+
+echo "sorted(...)"
 python -m timeit --setup $SETUP -- "a, b = sorted([a, b])" $ASSERT
+
+echo "check and flip"
 python -m timeit --setup $SETUP -- "if b < a:" "  b, a = a, b" $ASSERT
 
 # 2024-01-25
