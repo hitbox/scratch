4d93a4f1c1688a1094c0e09116c5323d9fb98c97
Carl Harris <hitbox@ryzen.com>
2024-05-16 12:24:25 -0400

    implemented randomize argument

--- twosum.py
+++ twosum.py
@@ -1,13 +1,13 @@
-# https://leetcode.com/problems/two-sum/description/
 import argparse
 import random
 
 def twosum(nums, target):
     pairs = {}
     for i, n in enumerate(nums):
+        d = target - n
         print(locals())
-        if target - n in pairs:
-            return [i, pairs[target - n]]
+        if d in pairs:
+            return (i, pairs[d])
         pairs[n] = i
 
 def main(argv=None):
@@ -17,7 +17,12 @@ def main(argv=None):
     parser.add_argument('--randomize', action='store_true')
     args = parser.parse_args(argv)
 
+    if args.randomize:
+        random.shuffle(args.nums)
+
     print(twosum(args.nums, args.target))
 
 if __name__ == '__main__':
     main()
+
+# https://leetcode.com/problems/two-sum/description/
