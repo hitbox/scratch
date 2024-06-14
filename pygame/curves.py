2ddf7f2626b9bcc523e0e9fcb16169581d2136ae
Carl Harris <hitbox@ryzen.com>
2024-06-14 01:40:31 -0400

    init curves.py

---
+++ pygame/curves.py
@@ -0,0 +1,5 @@
+# 2024-06-14 Fri.
+# https://github.com/dgmjs/dgmjs/blob/main/packages/core/src/utils/points-on-curve/README.md
+# https://github.com/pshihn/bezier-points/blob/master/src/index.ts
+# https://seant23.wordpress.com/2010/11/12/offset-bezier-curves/
+# Lots of links to interesting curve things as I browse through from the top link.
