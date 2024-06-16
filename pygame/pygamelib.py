40d01cc14823c2b68036a168ceb82df03804e870
Carl Harris <hitbox@ryzen.com>
2024-06-16 12:13:18 -0400

    pygamelib: midpoint func

--- pygame/pygamelib.py
+++ pygame/pygamelib.py
@@ -2239,6 +2239,9 @@ def ccw_side_lines_from_point(rect, point):
     for i in range(i, i+4):
         yield ccw_sides[i % 4]
 
+def midpoint(p1, p2):
+    return ((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2)
+
 def get_digits(s):
     # TODO: move this func
     for char in s:
