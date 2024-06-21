5e4805d1b90c385bc0138cd72fa6263a6e4ac46c
Carl Harris <hitbox@ryzen.com>
2024-06-21 06:19:20 -0400

    pygame/decor.py
    - interesting decorations on/around shapes

---
+++ pygame/decor.py
@@ -0,0 +1,158 @@
+import math
+
+import pygamelib
+
+from pygamelib import pygame
+
+class Pen:
+
+    def __init__(self, position=None, angle=None):
+        if position is None:
+            position = (0,0)
+        self.position = pygame.Vector2(position)
+        if angle is None:
+            angle = 0
+        self.angle = angle
+
+    def copy(self):
+        return self.position.copy()
+
+    def move(self, length):
+        radians = math.radians(self.angle)
+        direction = pygame.Vector2(math.cos(radians), -math.sin(radians)) * length
+        self.position += direction
+
+    def turn(self, angle):
+        self.angle = (self.angle + angle) % 360
+
+    def right(self):
+        self.turn(-90)
+
+    def left(self):
+        self.turn(90)
+
+
+def corner_lines(rect, side_portion):
+    x, y, w, h = rect
+    r = x + w
+    b = y + h
+    horizontal = w * side_portion
+    vertical = h * side_portion
+    # clockwise
+    # left side, top, line
+    yield ((x, y + vertical), (x, y))
+    # top side, left, line
+    yield ((x, y), (x + horizontal, y))
+    # top side, right, line
+    yield ((r - horizontal, y), (r, y))
+    # right side, top, line
+    yield ((r, y), (r, y + vertical))
+    # right side, bottom, line
+    yield ((r, b - vertical), (r, b))
+    # bottom side, right, line
+    yield ((r, b), (r - horizontal, b))
+    # bottom side, left, line
+    yield ((x + horizontal, b), (x, b))
+    # left side, bottom, line
+    yield ((x, b), (x, b - vertical))
+
+def corner_border(pen, portion, horizontal, vertical):
+    for side_length in [horizontal, vertical, horizontal, vertical]:
+        # starting in a corner
+        p1 = pen.copy()
+
+        pen.move(side_length * portion)
+        p2 = pen.copy()
+        yield (p1, p2)
+
+        # skip
+        pen.move(side_length - portion * side_length * 2)
+
+        p1 = pen.copy()
+        pen.move(side_length * portion)
+        p2 = pen.copy()
+        yield (p1, p2)
+
+        # turn right
+        pen.turn(-90)
+
+def inner_corner_border(pen, portion, horizontal, vertical):
+    # TODO
+    # - a min and max width so that little and big rects look good
+    # starting topleft facing right
+    pen.move(horizontal * portion)
+    for side in [horizontal, vertical, horizontal, vertical]:
+        p1 = pen.copy()
+        pen.move(side - portion * side * 2)
+        p2 = pen.copy()
+        yield (p1, p2)
+
+        p1 = pen.copy()
+        pen.right()
+        pen.move(side * portion)
+        p2 = pen.copy()
+        yield (p1, p2)
+
+        pen.left()
+        pen.move(side * portion)
+        p3 = pen.copy()
+        yield (p2, p3)
+
+        pen.right()
+
+def main(argv=None):
+    """
+    Draw decorations from basic shapes.
+    """
+    parser = pygamelib.command_line_parser(
+        description = main.__doc__,
+    )
+    args = parser.parse_args(argv)
+
+    window = pygame.Rect(0, 0, *args.display_size)
+    shrink = 0.90
+    rect = window.inflate(-shrink * window.width, -shrink * window.height)
+
+    horizontal = rect.width - 1
+    vertical = rect.height - 1
+
+    pen = Pen(position=rect.topleft)
+    portion = 0.15
+
+    # TODO
+    # - interactively create lines
+
+    lines = list(inner_corner_border(pen, portion, horizontal, vertical))
+
+    colors = (
+        'red',
+        'green',
+        'blue',
+        'brown',
+        'purple',
+        'orange',
+        'teal',
+        'indigo',
+        'yellow',
+        'olive',
+        'fuchsia',
+        'navy',
+    )
+
+    screen = pygame.display.set_mode(window.size)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key in (pygame.K_ESCAPE, pygame.K_q):
+                    pygamelib.post_quit()
+        screen.fill('black')
+        pygame.draw.rect(screen, 'grey10', rect, 1)
+        for (p1, p2), color in zip(lines, colors):
+            pygame.draw.line(screen, color, p1, p2)
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    main()
