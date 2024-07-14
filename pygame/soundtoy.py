f5834b77fa0de566214d113b3a0988b0460ff16b
Carl Harris <hitbox@ryzen.com>
2024-07-14 15:24:01 -0400

    pygame/soundtoy.py
    - begin playing with music macro language
    - searching for a good music notation format that lets me make sound
      effects and music

--- pygame/soundtoy.py
+++ pygame/soundtoy.py
@@ -11,12 +11,38 @@ from pygamelib import pygame
 # Note names in an octave, in the order of their semitone positions
 NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
-# samples per second
-sample_rate = 44100
-# seconds
-duration = 0.1
 # frequency of the sine wave (A4 note)
 A4_frequency = 440.0
+
+def parse_mml(mml, octave=4, length=0.25, tempo=120):
+    """
+    :param octave:
+    :param length: default to quarter note
+    :param tempo:
+    """
+    # music macro language
+    notes = []
+    i = 0
+    while i < len(mml):
+        char = mml[i]
+        if char == 'T':
+            i += 1
+            tempo = int(mml[i:i+3])
+            i += 2
+        elif char == 'L':
+            i += 1
+            length = 4 / int(mml[i])
+        elif char == 'O':
+            i += 1
+            octave = int(mml[i])
+        elif char in 'CDEFGAB':
+            note_name = char
+            if i+1 < len(mml) and mml[i+1] == '#':
+                note_name += '#'
+                i += 1
+            notes.append((note_name, octave, length * 60 / tempo))
+        i += 1
+    return notes
 
 def calculate_frequency(note_name, octave):
     note_index = NOTES.index(note_name)
@@ -28,14 +54,15 @@ def calculate_frequency(note_name, octave):
 def sine_sample(amplitude, frequency, n, sample_rate):
     return amplitude * math.sin(math.tau * frequency * n / sample_rate)
 
-def generate_wav(duration, frequency, sample_func):
-    num_samples = int(sample_rate * duration)
-
-    amplitude = 32767
+def generate_wav(sound_sequence, sample_func, sample_rate):
     data = bytearray()
-    for n in range(num_samples):
-        sample = sample_func(amplitude, frequency, n, sample_rate)
-        data.extend(struct.pack('<h', int(sample)))
+    for note, octave, duration in sound_sequence:
+        frequency = calculate_frequency(note, octave)
+        num_samples = int(sample_rate * duration)
+        amplitude = 32767
+        for n in range(num_samples):
+            sample = sample_func(amplitude, frequency, n, sample_rate)
+            data.extend(struct.pack('<h', int(sample)))
 
     wav_buffer = io.BytesIO()
     with wave.open(wav_buffer, 'wb') as wav_file:
@@ -47,11 +74,10 @@ def generate_wav(duration, frequency, sample_func):
     wav_buffer.seek(0)
     return wav_buffer
 
-def run(args):
+def run(notes, args):
     pygame.mixer.init()
 
-    frequency = calculate_frequency(args.note, args.octave)
-    wav_buffer = generate_wav(args.duration, frequency, sine_sample)
+    wav_buffer = generate_wav(notes, sine_sample, args.sample_rate)
     sound = pygame.mixer.Sound(wav_buffer)
 
     screen = pygame.display.set_mode((800,)*2)
@@ -66,23 +92,31 @@ def run(args):
                 elif event.key == pygame.K_SPACE:
                     sound.play()
 
-def main(argv=None):
+def argument_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'note',
-        choices = NOTES,
-    )
-    parser.add_argument(
-        'octave',
-        type = int,
+        '--mml',
     )
     parser.add_argument(
         '--duration',
         type = float,
         default = 0.1,
     )
+    parser.add_argument(
+        '--sample-rate',
+        type = int,
+        default = 44100,
+    )
+    return parser
+
+def main(argv=None):
+    parser = argument_parser()
     args = parser.parse_args(argv)
-    run(args)
+    if args.mml:
+        notes = parse_mml(args.mml)
+        print(notes)
+        return
+        run(notes, args)
 
 if __name__ == '__main__':
     main()
