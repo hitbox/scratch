3281921ade6a59d2c3175e81529ffb711d996b8e
Carl Harris <hitbox@ryzen.com>
2024-07-31 01:33:37 -0400

    pygame/rule110.py
    init rule 110 animation

---
+++ pygame/rule110.py
@@ -0,0 +1,5 @@
+# 2024-07-31 Wed.
+# - reading this:
+#   https://ogiekako.vercel.app/blog/find_mkdir_tc
+# - https://en.wikipedia.org/wiki/Rule_110
+# - Implement Rule 110 animation with pygame or something.
