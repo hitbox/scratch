81ad5a7599ebbb84a793945e915ea540960b7533
Carl Harris <hitbox@ryzen.com>
2024-08-11 20:31:55 -0400

    init pyzzfx.py

---
+++ pyzzfx.py
@@ -0,0 +1,6 @@
+# 2024-08-11 Sun.
+# - Reading this:
+#   https://frankforce.com/space-huggers-how-i-made-a-game-in-13-kilobytes/
+# - Mentions this:
+#   https://github.com/KilledByAPixel/ZzFX/tree/master
+# - Want to make a Python version.
